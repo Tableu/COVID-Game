90ab8ce6a2f1b420a12dc4c4cf36e57acd63a92d
darthvader99999 <bhow245@gmail.com>
2020-10-24 13:05:28 -0700

    Added a progress bar that shows how exposed the player is: - Added a canvas and slider bar in Test Level - Added script FillBar with function CurrentValue which allows scripts to set the sliders value. - Added code to PlayerHealth which updates the slider bar when exposure changes

---
+++ Assets/Scripts/UI/FillBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillBar : MonoBehaviour
+{
+    public Slider slider;
+
+    private float currentValue = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentValue = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public float CurrentValue {
+        get {
+            return currentValue;
+        }
+        set {
+            currentValue = value;
+            slider.value = currentValue;
+        }
+    }
+}

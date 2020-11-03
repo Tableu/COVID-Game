901f400593cb32d593a2bb2e60953b3caa00ddd7
darthvader99999 <bhow245@gmail.com>
2020-11-02 23:08:13 -0800

    Added function to PathAlgorithm and created EnemyPathing: - Implemented a basic path algorithm which finds a path of valid tiles between two points on the map. - Added GetMoves to PathAlgorithm, which returns a list of valid moves for FindPath to explore. - Created EnemyPathing which uses MoveTowards to move the enemy between each tile on its assigned path.

---
+++ Assets/Scripts/Enemy/EnemyPathing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.Tilemaps;
+
+public class EnemyPathing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Vector3> path;
+    public float enemySpeed;
+    private int index;
+    void Start()
+    {
+        index = 1;
+    }
+
+    // Update is called once per frame
+    void Update() //Move the enemy towards each step in the path.
+    {
+        if (index >= path.Count) return;
+        transform.position = Vector3.MoveTowards(transform.position, path[index], 0.001f);
+        if (transform.position.Equals(path[index]))
+            index++;
+    }
+}
--- Assets/Scripts/PathAlgorithm.cs
+++ Assets/Scripts/PathAlgorithm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -9,36 +10,70 @@ public class PathAlgorithm : MonoBehaviour
     public Tilemap tileMap = null;
     public List<String> validTiles;
     public List<Vector3Int> availableTiles;
-
+    public GameObject enemyPrefab;
     private Vector3Int[,] tileGrid;
     // Start is called before the first frame update
     void Start()
     {
         tileMap = GetComponent<Tilemap>();
         availableTiles = new List<Vector3Int>();
-        tileGrid = new Vector3Int[tileMap.cellBounds.size.y,tileMap.cellBounds.size.x];
-        int index = 0;
-        int rowSize = tileMap.cellBounds.size.x;
+        //Sets up colliders for buildings
         foreach (var position in tileMap.cellBounds.allPositionsWithin) {
             if (!tileMap.HasTile(position)) {
                 continue;
             }
-            
             if (validTiles.Contains((tileMap.GetSprite(position).name))){
                 availableTiles.Add(position);
-                tileGrid[index/rowSize, index%rowSize] = position;
                 tileMap.SetColliderType(position,Tile.ColliderType.None);
             }else{
                 tileMap.SetColliderType(position,Tile.ColliderType.Grid);
             }
-
-            index++;
+            
         }
+        var tilePath = FindPath(new List<Vector3Int>(),availableTiles[1],availableTiles[2]);
+        var enemy = Instantiate(enemyPrefab, tileMap.CellToWorld(tilePath[0]), Quaternion.identity);
+        var worldPath = new List<Vector3>();
+        foreach (var tile in tilePath){
+            worldPath.Add(tileMap.CellToWorld(tile));
+        }
+        enemy.GetComponent<EnemyPathing>().path = worldPath;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    //Basic algorithm that returns a path between two points. Returns null if no path is found.
+    private List<Vector3Int> FindPath(List<Vector3Int> path,Vector3Int start, Vector3Int end)
+    {
+        path.Add(start);
+        if (start.Equals(end))
+            return path;
+        var moves = GetMoves(start);
+        foreach (var move in moves)
+        {
+            if (path.Contains(move))
+                continue;
+            var result = FindPath(path, move, end);
+            if (result != null)
+                return result;
+        }
+        return null;
+    }
+    //Returns a list of neighboring tiles to the indicated position.
+    private List<Vector3Int> GetMoves(Vector3Int position){
+        var moves = new List<Vector3Int>(){
+            new Vector3Int(position.x-1,position.y,position.z),new Vector3Int(position.x,position.y+1,position.z),
+            new Vector3Int(position.x,position.y-1,position.z),new Vector3Int(position.x+1,position.y,position.z)
+        };
+        var validMoves = new List<Vector3Int>();
+        foreach (var move in moves){
+            if(tileMap.HasTile(move)){
+                if(validTiles.Contains((tileMap.GetSprite(move).name)))
+                    validMoves.Add(move);
+            }
+        }
+        return validMoves;
     }
 }
